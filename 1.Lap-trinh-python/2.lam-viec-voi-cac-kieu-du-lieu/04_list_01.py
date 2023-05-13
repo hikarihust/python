c087d2bd202e8abf3a8f2f4739f414f3a1a04c53
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-14 02:28:18 +0900

    List 02

--- 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/04_list_01.py
+++ 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/04_list_01.py
@@ -11,3 +11,36 @@ myList = [1, 2, 3, "a", "b", "c", ["d", "e"]]
 print(myList[0])
 print(myList[-1])
 print(myList[6][1])
+
+# Trích xuất
+myList = ["a", "b", "c"]
+print(myList[2:])
+print(myList[0:2])
+
+# Cập nhật phần tử
+myList = ["a", "b", "c"]
+myList[0] = "Tada"
+print(myList)
+
+# Xóa phần tử
+myList = ["a", "b", "c"]
+del myList[0]
+print(myList)
+
+# append
+myList = ["a", "b", "c"]
+myList.append("Python")
+print(myList)
+
+myList = ["a", "b", "c"]
+myList.append(["x", "y"])
+print(myList)
+
+myList = ["a", "b", "c"]
+myList.extend(["x", "y"])
+print(myList)
+#pop
+myList = ["a", "b", "c"]
+item = myList.pop(1)
+print(item)
+print(myList)
