0d98dd6821bc94993c79eab92194e27fdd65302d
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-14 01:55:12 +0900

    List 01

---
+++ 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/04_list_01.py
@@ -0,0 +1,13 @@
+myList = ["a", "b", "c"]
+myList = [1, 2, 3]
+myList = [1, 2, 3, "a", "b", "c", ["d", "e"]]
+
+# Kiểm tra sự tồn tại
+print("a" in myList)
+print("aA" not in myList)
+
+# Index
+myList = [1, 2, 3, "a", "b", "c", ["d", "e"]]
+print(myList[0])
+print(myList[-1])
+print(myList[6][1])
