be1d0e72ccb9b989cac3325e5a2710e1d65c4765
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-13 16:46:24 +0900

    Number

---
+++ 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/02_number.py
@@ -0,0 +1,14 @@
+# Number các thao tác cơ bản
+result1 = 1 + 2
+result2 = 10 + 2 * 100 - 50
+
+print(result1)
+print(result2)
+
+# Ép kiểu
+result2 = int(2.8)
+result3 = float("2.3")
+
+print(result2)
+print(result3)
+print(type(result3))
