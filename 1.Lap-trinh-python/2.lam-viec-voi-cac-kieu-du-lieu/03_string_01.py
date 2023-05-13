f061c72c3027ede4ae0bc533c9df253a68f09e81
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-13 20:23:00 +0900

    String 02

--- 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/03_string_01.py
+++ 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/03_string_01.py
@@ -32,3 +32,18 @@ print(result5)
 print(result6)
 
 # Thay đổi giá trị của phần tử trong chuỗi: str1[0] = "d" không thể 
+
+# Trích xuất chuỗi
+myStr   = "hello python"
+result7 = myStr[:]  # Cũng giốngg với result7 = myStr
+print(result7)
+result7 = myStr[2:] # from:to-1 -> Lấy từ chỉ số 2 cho đến cuối chuỗi
+print(result7)
+result7 = myStr[:2] # Lấy từ chỉ số 0 cho đến chỉ số 1
+print(result7)
+result7 = myStr[2:4]  # Lấy từ chỉ số 2 cho đến chỉ số 3
+print(result7)
+result7 = myStr[::3] # Không truyền from và to, có bước nhảy là 3 -> hlph
+print(result7)
+result7 = myStr[::-1]  # đảo ngược chuỗi
+print(result7)
