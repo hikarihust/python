844d91e559940932c0b0b449f65593c3f0f976a3
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-13 21:00:01 +0900

    String 03

---
+++ 1.Lap-trinh-python/2.lam-viec-voi-cac-kieu-du-lieu/03_string_02.py
@@ -0,0 +1,16 @@
+str1 = "quangvu:18:170cm" #name:age:height
+str2 = "pyTHon is easy"
+
+result1 = str1.split(":")
+print(result1)
+print(result1[1])
+result2 = str2.upper()
+print(result2)
+result3 = str2.lower()
+print(result3)
+result4 = str2.swapcase()
+print(result4)
+result5 = str2.title()
+print(result5)
+result6 = str2.replace("p", "HAHA")
+print(result6)
