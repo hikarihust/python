edb86c67e4290ff54cdba9dcf4501a260ebb8350
Vu Dinh Quang <vudinhquangk53@gmail.com>
2023-05-14 12:19:35 +0900

    Break Continue

---
+++ 1.Lap-trinh-python/3.controll-Statements-if-elseif-while-do-forloop/04_break_continue.py
@@ -0,0 +1,15 @@
+# break
+# i = 0
+# while i <= 5:
+#     if(i % 2 == 0):
+#         print(i)
+#         break
+#     i = i + 1
+
+# continue
+i = 0
+while i <= 5:
+    print(i)
+    i = i + 1
+    continue
+    print("End")
